a50fae7ebb1f07498d4de084680878507b4dfb8e
linkgs <40344950+linkgs@users.noreply.github.com>
2020-12-25 20:36:52 +0800

    Update diy5.4.sh

--- diy5.4.sh
+++ diy5.4.sh
@@ -15,8 +15,12 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
+#!/bin/bash
+#=================================================
+# Description: Build OpenWrt using GitHub Actions
+rm -rf ./package/lean/luci-theme-argon
 #git clone https://github.com/firker/diy-ziyong package/diy-ziyong
-#sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
+sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git szpkge https://github.com/281677160/openwrt-package' feeds.conf.default
 #git clone https://github.com/281677160/openwrt-package.git package/szpkge
